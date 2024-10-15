866d32ede407952723720d7b39878aeed6dc65ff
Daniel Dolomount <dolomountd@gmail.com>
2024-10-15 19:53:52 -0230

    even more testing

--- services/reset-demo.php
+++ services/reset-demo.php
@@ -2,8 +2,8 @@
 
 $_POST["authorize"] = "gradeplus";
 
-header('Content-Type: application/json');
-    echo json_encode(["success" => 10,"error" => 11,"illegal" => 0]);
+$success = 10;
+$error = 11;
 
 // Service to initialize/reset demo database. Handles creating MySQL user "gradeplusclient", creating "gradeplus" database, creating and filling "login" table.
 if ($_POST["authorize"] == "gradeplus") {
